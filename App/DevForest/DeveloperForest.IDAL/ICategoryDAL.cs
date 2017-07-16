8ed7059f6fe0104f37fbb36e695cf65d03e0fb0b
Saurabh Singh <saurabh.pithoragarh.uk@gmail.com>
2017-07-16 13:52:42 +0530

    IDAL

---
+++ App/DevForest/DeveloperForest.IDAL/ICategoryDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeveloperForest.Model;
+
+namespace DeveloperForest.IDAL
+{
+    public interface ICategoryDAL
+    {
+        int InsertCategory(CategoryModel model);
+        int InsertSubCategory(CategoryModel model);
+
+        CategoryModel CategoryList();
+    }
+}
