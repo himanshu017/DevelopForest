917bbb5a06b7bb1040fd0b7f9c2940be8bffd7f7
Saurabh Singh <saurabh.pithoragarh.uk@gmail.com>
2017-07-16 13:53:25 +0530

    bll

---
+++ App/DevForest/DeveloperForest.BLL/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeveloperForest.IDAL;
+using DeveloperForest.DAL;
+using DeveloperForest.Model;
+
+namespace DeveloperForest.BLL
+{
+    public class Category
+    {
+        public int InsertCategory(CategoryModel model)
+        {
+            IDAL.ICategoryDAL obj = new CategoryDAL();
+            return obj.InsertCategory(model);
+        }
+
+        public int InsertSubCategory(CategoryModel model)
+        {
+            ICategoryDAL obj = new CategoryDAL();
+            return obj.InsertSubCategory(model);
+        }
+    }
+}
