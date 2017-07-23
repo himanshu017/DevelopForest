f680b03b0f49e2b14387dd6721403c5a053f8aa6
Saurabh Singh <saurabh.pithoragarh.uk@gmail.com>
2017-07-23 11:55:24 +0530

    Controller

--- App/DevForest/DevForest/Controllers/CategoryController.cs
+++ App/DevForest/DevForest/Controllers/CategoryController.cs
@@ -11,10 +11,6 @@ namespace DevForest.Controllers
     {
         //
         // GET: /Category/
-        public ActionResult Index()
-        {
-            return View();
-        }
 
         public ActionResult Category()
         {
@@ -37,23 +33,21 @@ namespace DevForest.Controllers
 
             int res = obj.DeleteCategoryByID(CategoryID);
 
-            return Json(res,JsonRequestBehavior.AllowGet);
+            return Json(res, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult SubCategory()
         {
-
-            return View();
+            CategoryHelper obj = new CategoryHelper();
+            CategoryModel ob = new CategoryModel();
+            ob.CategoryList = new SelectList(obj.GetCategories(), "CategoryId", "CategoryName");
+            return View(ob);
         }
         public ActionResult InsertSubCategory(CategoryModel model)
         {
             CategoryHelper obj = new CategoryHelper();
-            int result=obj.InsertSubCategory(model);
-            if (result > 0)
-            {
-                return RedirectToAction("SubCategory");
-            }
-            return View();
+            int result = obj.InsertSubCategory(model);
+            return Json(result.ToString(), JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult GetCategories(string order, int limit = 0, int offset = 0, string search = null, string sort = null)
@@ -103,5 +97,65 @@ namespace DevForest.Controllers
                 return Json(new { Result = "ERROR", Message = ex.Message });
             }
         }
+
+        public JsonResult GetSubCategories(string order, int limit = 0, int offset = 0, string search = null, string sort = null)
+        {
+            try
+            {
+                CategoryHelper obj = new CategoryHelper();
+                int count = 0;
+                var Main = obj.GetSubCategories();
+                if (!string.IsNullOrWhiteSpace(search))
+                {
+
+                    List<DeveloperForest.Model.CategoryModel> li = Main.Where(x => x.SubCategoryName.ToUpper().Contains(search.ToUpper())).ToList();
+                    count = li.Count;
+                    li = li.OrderBy(s => s.SubCategoryName).Skip(offset).Take(limit == 0 ? count : limit).ToList();
+                    return Json(new { rows = li });
+                }
+                else
+                {
+                    count = Main.Count;
+                    List<DeveloperForest.Model.CategoryModel> li = Main;
+                    List<DeveloperForest.Model.CategoryModel> li2 = new List<DeveloperForest.Model.CategoryModel>();
+                    if (sort != null)
+                    {
+                        var param = sort;
+                        var propertyInfo = typeof(DeveloperForest.Model.CategoryModel).GetProperty(param);
+
+                        if (order == "asc")
+                        {
+                            li2 = li.OrderBy(x => propertyInfo.GetValue(x, null)).Skip(offset).Take(limit == 0 ? count : limit).ToList();
+                        }
+                        else
+                        {
+                            li2 = li.OrderByDescending(x => propertyInfo.GetValue(x, null)).Skip(offset).Take(limit == 0 ? count : limit).ToList();
+                        }
+                    }
+                    else
+                    {
+                        li2 = li.OrderBy(x => x.SubCategoryName).Skip(offset).Take(limit == 0 ? count : limit).ToList();
+                    }
+
+                    return Json(new { total = count, rows = li2 });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Result = "ERROR", Message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public JsonResult DeleteSubCategoryByID(int SubCategoryID)
+        {
+            CategoryHelper obj = new CategoryHelper();
+
+            int res = obj.DeleteSubCategoryByID(SubCategoryID);
+
+            return Json(res, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
