e95f864f134f7459950a955e20985093da41b024
Saurabh Singh <saurabh.pithoragarh.uk@gmail.com>
2017-07-23 11:49:32 +0530

    Model

---
+++ App/DevForest/DeveloperForest.Model/ThemeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DeveloperForest.Model
+{
+    public class ThemeModel:Base
+    {
+        public int ThemeId { get; set; }
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public int SubCategoryId { get; set; }
+
+        public string SubCategoryName { get; set; }
+        public string Title { get; set; }
+        public string ImageName { get; set; }
+        public string Description { get; set; }
+        public string Downloadlink { get; set; }
+        public string DemoLink { get; set; }
+
+
+    }
+}
