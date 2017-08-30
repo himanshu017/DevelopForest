e3eff4e9d08ac3b4d89129e180ef9f81f7a03737
Saurabh Singh <saurabh.pithoragarh.uk@gmail.com>
2017-08-30 20:09:49 +0530

    sss

--- App/DevForest/DeveloperForest.DAL/ThemeDAL.cs
+++ App/DevForest/DeveloperForest.DAL/ThemeDAL.cs
@@ -34,6 +34,11 @@ namespace DeveloperForest.DAL
                 db.AddInParameter(dbCommand, "CreatedBy", DbType.String, model.CreatedBy);
                 db.AddInParameter(dbCommand, "ModifiedBy", DbType.String, model.ModifiedBy);
                 db.AddInParameter(dbCommand, "ImageName", DbType.String, model.ImageName);
+                db.AddInParameter(dbCommand, "RelatedLink1", DbType.String, model.RelatedLink1);
+                db.AddInParameter(dbCommand, "RelatedLink2", DbType.String, model.RelatedLink2);
+                db.AddInParameter(dbCommand, "RelatedLink3", DbType.String, model.RelatedLink3);
+                db.AddInParameter(dbCommand, "RelatedLink4", DbType.String, model.RelatedLink4);
+                db.AddInParameter(dbCommand, "RelatedLink5", DbType.String, model.RelatedLink5);
                 con.Open();
                 return db.ExecuteNonQuery(dbCommand);
             }
@@ -61,18 +66,24 @@ namespace DeveloperForest.DAL
                 Description = String.IsNullOrEmpty(row.Field<string>("Description")) ? "" : row.Field<string>("Description"),
                 ImageName = String.IsNullOrEmpty(row.Field<string>("ImageName")) ? "" : row.Field<string>("ImageName"),
                 PreImageName = String.IsNullOrEmpty(row.Field<string>("ImageName")) ? "" : row.Field<string>("ImageName"),
+                BaseURL = ConfigurationManager.AppSettings["BaseURL"].ToString(),
+                RelatedLink1 = String.IsNullOrEmpty(row.Field<string>("RelatedLink1")) ? "" : row.Field<string>("RelatedLink1"),
+                RelatedLink2 = String.IsNullOrEmpty(row.Field<string>("RelatedLink2")) ? "" : row.Field<string>("RelatedLink2"),
+                RelatedLink3 = String.IsNullOrEmpty(row.Field<string>("RelatedLink3")) ? "" : row.Field<string>("RelatedLink3"),
+                RelatedLink4 = String.IsNullOrEmpty(row.Field<string>("RelatedLink4")) ? "" : row.Field<string>("RelatedLink4"),
+                RelatedLink5 = String.IsNullOrEmpty(row.Field<string>("RelatedLink5")) ? "" : row.Field<string>("RelatedLink5")
             }).ToList();
             return ThemeList;
         }
 
         public string DeleteThemeById(int id)
         {
-                DbCommand dbCommand = db.GetStoredProcCommand("sp_DeleteThemeById");
-                db.AddInParameter(dbCommand, "ThemeId", DbType.Int16, id);
-                db.AddOutParameter(dbCommand, "ImageName", DbType.String, 300);
-                var ImageName=db.ExecuteScalar(dbCommand);
-                return ImageName.ToString();
-            
+            DbCommand dbCommand = db.GetStoredProcCommand("sp_DeleteThemeById");
+            db.AddInParameter(dbCommand, "ThemeId", DbType.Int16, id);
+            db.AddOutParameter(dbCommand, "ImageName", DbType.String, 300);
+            var ImageName = db.ExecuteScalar(dbCommand);
+            return ImageName.ToString();
+
         }
     }
 }
